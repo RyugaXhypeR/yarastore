5b5fc29de10eee9f701fb3d9c533bb01ef04e5ef
RyugaXhypeR <ryugashyper@gmail.com>
2024-08-29 18:03:55 +0000

    faet: added docs and `makePathAbs`

--- pkg/config/load.go
+++ pkg/config/load.go
@@ -8,42 +8,92 @@ import (
 	"yarastore/pkg/utils"
 )
 
+// ConfigValues Common configure options for `Rules` and `Target` files
 type ConfigValues struct {
-	Dirs           []string `toml:"dirs"`
-	Files          []string `toml:"files"`
-	Exclude        []string `toml:"exclude"`
-	IncludePattern string   `toml:"include_pattern"`
-	ExcludePattern string   `toml:"exclude_pattern"`
+	// The directories to scan.
+	Dirs []string `toml:"dirs"`
+	// The files to scan.
+	Files []string `toml:"files"`
+	// The files to exclude.
+	Exclude []string `toml:"exclude"`
+	// Consider files if they have this pattern.
+	IncludePattern string `toml:"include_pattern"`
+	// Discard files if they have this pattern.
+	ExcludePattern string `toml:"exclude_pattern"`
 }
 
+// Config Toml config options for the app.
 type Config struct {
-	Rules  ConfigValues `toml:"rules"`
+	// ConfigValues for yara-rules.
+	Rules ConfigValues `toml:"rules"`
+	// ConfigValues for the files to match yara-rules against.
 	Target ConfigValues `toml:"target"`
 }
 
+// LoadConfig Load toml config from a file.
 func LoadConfig(filename string) (*Config, error) {
 	var config Config
 	_, err := toml.DecodeFile(filename, &config)
-	return &config, err
+	if err != nil {
+		return nil, err
+	}
+	if err := config.Rules.makePathAbs(); err != nil {
+		return nil, err
+	}
+	if err := config.Target.makePathAbs(); err != nil {
+		return nil, err
+	}
+	return &config, nil
 }
 
-func (c *ConfigValues) IsFilenameValid(path string) bool {
-	filename := filepath.Base(path)
+// makePathAbs Convert all patterns and exclude files to absolute paths
+// to make matching patterns easier.
+func (c *ConfigValues) makePathAbs() error {
+	// Convert all files in `Exclude` to absolute paths.
+	for i, file := range c.Exclude {
+		absPath, err := filepath.Abs(file)
+		if err != nil {
+			return err
+		}
+		c.Exclude[i] = absPath
+	}
+
 	if c.IncludePattern != "" {
-		match, err := filepath.Match(c.IncludePattern, filename)
+		absPath, err := filepath.Abs(c.IncludePattern)
+		if err != nil {
+			return err
+		}
+		c.IncludePattern = absPath
+	}
+
+	if c.ExcludePattern != "" {
+		absPath, err := filepath.Abs(c.ExcludePattern)
+		if err != nil {
+			return err
+		}
+		c.ExcludePattern = absPath
+	}
+
+	return nil
+}
+
+// IsFilenameValid A predicate which applies filters to check given `path` is valid.
+func (c *ConfigValues) IsFilenameValid(path string) bool {
+	if c.IncludePattern != "" {
+		match, err := filepath.Match(c.IncludePattern, path)
 		if !match || err != nil {
 			return false
 		}
 	}
 
 	if c.ExcludePattern != "" {
-		match, err := filepath.Match(c.ExcludePattern, filename)
+		match, err := filepath.Match(c.ExcludePattern, path)
 		if match || err != nil {
 			return false
 		}
 	}
 
-	if utils.SliceContains(c.Exclude, filename) {
+	if utils.FileHasPrefix(c.Exclude, path) {
 		return false
 	}
 
