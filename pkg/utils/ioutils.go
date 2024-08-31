969400fc742ef3783318841dd18c0717a995ce92
RyugaXhypeR <ryugashyper@gmail.com>
2024-08-31 10:47:07 +0000

    feat: removed dead batteries (`ListDirWithPred`)

--- pkg/utils/ioutils.go
+++ pkg/utils/ioutils.go
@@ -1,24 +1,8 @@
 package utils
 
 import (
-	"os"
-	"path/filepath"
 	"strings"
 )
-
-func ListDirWithPred(dirname string, predicate func(string, os.FileInfo) bool) ([]string, error) {
-	var files []string
-	err := filepath.Walk(dirname, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-		if !info.IsDir() && predicate(path, info) {
-			files = append(files, path)
-		}
-		return nil
-	})
-	return files, err
-}
 
 func FileContains(excludeComponents []string, filepath string) bool {
 	for _, component := range excludeComponents {
